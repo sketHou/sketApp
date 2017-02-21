0faa5623b996a383fd8c6a96f5cc392f97b69844
sket <skethou@lagou.com>
2017-02-21 17:20:38 +0800

    引icons 做底部导航栏

--- index.ios.js
+++ index.ios.js
@@ -8,13 +8,16 @@ import React, { Component } from 'react';
 import { AppRegistry , Text} from 'react-native';
 import {Scene, Router, NavBar, Actions, TabBar} from 'react-native-router-flux';
 import * as Pages from './src/index.js';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-class TabIcon extends Component {
-    render(){
-        return (
-            <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
-        );
-    }
+function TabIconFactory(nameString) {
+	return React.createClass({
+		render() {
+			return (
+				<Icon name={nameString} size={32} color={this.props.selected ? '#FFA07A' :'#333333'}></Icon>
+			);
+		}		
+	})
 }
 
 function backButton() {
@@ -26,9 +29,9 @@ export default class sketApp extends Component {
 	render() {
 		return <Router>
 			<Scene key="root" tabs={true} tabBarStyle={{backgroundColor: '#fff'}}>
-				<Scene key="index" icon={TabIcon} component={Pages.indexPage} title="indexPage" initial={true} hideNavBar={true}></Scene>
-				<Scene key="info" icon={TabIcon} component={Pages.infoPage} title="infoPage" hideNavBar={false}></Scene>
-				<Scene key="webview" icon={TabIcon} component={Pages.webviewPage} title="webviewPage" hideNavBar={false}></Scene>
+				<Scene key="index" icon={TabIconFactory('home')} component={Pages.indexPage} title="indexPage" initial={true} hideNavBar={true}></Scene>
+				<Scene key="info" icon={TabIconFactory('info-circle')} component={Pages.infoPage} title="infoPage" hideNavBar={false}></Scene>
+				<Scene key="webview" icon={TabIconFactory('link')} component={Pages.webviewPage} title="webviewPage" hideNavBar={false}></Scene>
 			</Scene>
 		</Router>
 	}
